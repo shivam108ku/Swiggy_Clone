71d5427b3640bf5b431b6550a39493bf4fb64ef6
shivam108ku <shivam108ku@gmail.com>
2025-04-23 15:41:14 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <header className='bg-[#FF5200]'>
+        <div>
+            <img 
+            className='w-40 h-12'
+            src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png" alt="" />
+        </div>
+        <div></div>
+        <div></div>
+
+     </header>
+  )
+}
+
+export default Header;
